1e2e25aa0f547f86e073f426ef1079cfac911f22
BormachBoroda <vano888888@gmail.com>
2025-04-30 15:53:20 +0300

    chore: not essential changes

--- backend/main.cpp
+++ backend/main.cpp
@@ -1,18 +1,28 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
+#include <QDebug>
 
 int main(int argc, char *argv[])
 {
     QGuiApplication app(argc, argv);
 
     QQmlApplicationEngine engine;
+
+    engine.addImportPath("E:/Qt/QtProjects/Roboblade/build/Desktop_Qt_6_6_3_MinGW_64_bit-Debug");
+    engine.addImportPath("E:/Qt/QtProjects/Roboblade/build/Desktop_Qt_6_6_3_MinGW_64_bit-Debug/frontend");
+    engine.addImportPath(":/");
+
+    for(const auto& path : engine.importPathList()) {
+        qDebug() << path;
+    }
+
     QObject::connect(
         &engine,
         &QQmlApplicationEngine::objectCreationFailed,
         &app,
         []() { QCoreApplication::exit(-1); },
         Qt::QueuedConnection);
-    engine.loadFromModule("Roboblade", "Main");
+    engine.loadFromModule("frontend", "Main");
 
     return app.exec();
 }
---
+++ frontend/Main.qml
@@ -0,0 +1,11 @@
+import QtQuick
+import QtQuick.Controls
+
+
+ApplicationWindow {
+  id: app
+
+  width: 1100; height: 650
+  title: qsTr("Roboblade")
+  visible: true
+}
